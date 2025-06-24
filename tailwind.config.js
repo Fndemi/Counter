fa8209b000b1dacafe20b52f294d368b2c7c9dc3
Florence Ndemi <ndemiflorence@gmail.com>
2025-06-24 15:46:06 +0300

    Commit:Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./build/*.html",        // Your HTML files in the build folder
+    "./src/**/*.js",         // All JS files inside src folder (including subfolders)
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+s
